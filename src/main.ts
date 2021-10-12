3c018d1440c3c65f7afa9daaa18ea0ecbba43a1a
Connor Bär <connor.baer@me.com>
2021-10-12 13:02:58 +0200

    Implement action

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,31 @@
 import * as core from '@actions/core';
-
-import { wait } from './wait';
+import * as github from '@actions/github';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds');
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const branch: string = core.getInput('branch');
+    const force: boolean = core.getInput('force') === 'true';
+    const token: string = core.getInput('token');
 
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
+    const octokit = github.getOctokit(token);
+    const { ref, repo, sha } = github.context;
 
-    core.setOutput('time', new Date().toTimeString());
+    if (ref === `refs/heads/${branch}`) {
+      const baseBranch = ref.replace(/^(refs\/heads\/)/, '');
+      core.warning(
+        `'${branch}' is already up to date with '${baseBranch}', skipping.`,
+      );
+      return;
+    }
+
+    core.debug(`Fast-forwarding '${branch}' to '${ref}'...`);
+
+    await octokit.rest.git.updateRef({
+      ...repo,
+      ref: `heads/${branch}`,
+      sha,
+      force,
+    });
   } catch (error) {
     core.setFailed((error as Error).message);
   }
